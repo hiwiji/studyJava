1f3d049c4487a32db8ee840bca6bff12111f8cfa
hiwiji <jjewwi12@gmail.com>
2023-05-08 23:50:57 +0900

    variable study

---
+++ ch02/src/VarEx2.java
@@ -0,0 +1,18 @@
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		
+		int x = 4, y = 2;
+		int tmp;
+		
+		tmp = x; // 1. x의 값을 tmp에 저장 // tmp는 4
+		x = y;   // 2. y의 값을 x에 저장 // x는 2 
+		y = tmp; // 3. tmp의 값을 y에 저장 // y는 4
+		
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+
+	}
+
+}
